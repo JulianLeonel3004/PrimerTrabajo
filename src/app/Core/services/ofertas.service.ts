d9f13da7421a929086a5bbb338f9724e01e41581
Julian <JLopez@sofrecom.com.ar>
2020-08-09 02:01:19 -0300

    firebase

--- src/app/Core/services/ofertas.service.ts
+++ src/app/Core/services/ofertas.service.ts
@@ -11,20 +11,12 @@ import { Oferta } from '../Modules/oferta';
         let oferta:Oferta = new Oferta();
         let ofertas:Array<Oferta> = new Array<Oferta>();
 
-        oferta.enviarCV = "bteijeiro@tgv.com.ar";
-        oferta.ubicacion = "Puerto Madero";
-        oferta.requisitos = "Conocimientos: .NET, C#, ASP.NET, Java, JavaScript/Jquery/POO/AJAX/HTML5, Web Forms, Web Servers, SQL Server, Oracle, etc. ";
-        oferta.empresa = "TGV";
-        oferta.puesto = "Desarrollador";
-        ofertas.push(oferta);
-
-
         oferta = new Oferta();
-        oferta.enviarCV = "pcerezo@pedemonteasoc.com.ar";
-        oferta.ubicacion = "Córdoba";
-        oferta.requisitos = "Idioma: Inglés y/o Italiano,	Full – Time,	Estudiantes avanzados o egresados de carreras informáticas";
-        oferta.empresa = "Empresa de Córdoba ( Reclutadora no especificó )";
-        oferta.puesto = "PROGRAMADOR  JAVA – BACK END / PROGRAMADOR ANGULAR  8 – FRONT END";
+        oferta.enviarCV = "";
+        oferta.ubicacion = "";
+        oferta.requisitos = "";
+        oferta.empresa = "";
+        oferta.puesto = "";
         ofertas.push(oferta);
 
         return ofertas;
