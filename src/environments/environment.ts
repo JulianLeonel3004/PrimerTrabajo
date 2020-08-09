6c4654c5f2ef191aa9531f826908f3c4ce8d71cb
Julian <JLopez@sofrecom.com.ar>
2020-08-09 02:32:55 -0300

    certificados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAww9eY1nL8RdxEAouZY_hYAJBIoCLJ-Sg",
-    authDomain: "miprimertrabajo-3bd80.firebaseapp.com",
-    databaseURL: "https://miprimertrabajo-3bd80.firebaseio.com",
-    projectId: "miprimertrabajo-3bd80",
-    storageBucket: "miprimertrabajo-3bd80.appspot.com",
-    messagingSenderId: "799639941136",
-    appId: "1:799639941136:web:13830043d0e46b179dda95",
-    measurementId: "G-D9XVPX1Y0L"
+    ///Los certificados de firebase no están disponibles por razones de seguridad
   }
 };
 
